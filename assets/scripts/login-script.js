a50edf1f0f1dd52a183779b9602038557ee38ee6
Im3NFP <116356371+brayone-no-xv@users.noreply.github.com>
2025-08-18 13:18:53 +0800

    Update login-script.js
    Mengganti komentar sesuai dengan format JDSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,6 +3,7 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ * Izin eksploitasi
  */
 
 const loginFormElement = document.querySelector('#loginForm');
